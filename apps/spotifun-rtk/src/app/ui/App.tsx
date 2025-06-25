32e1fba11c555ba42bc1064c6f9ae0a6c4dd58b8
Arina-Vas <mrs.a.vasilevskaya@gmail.com>
2025-06-25 18:08:38 +0200

    feat: add navBar

--- apps/spotifun-rtk/src/app/ui/App.tsx
+++ apps/spotifun-rtk/src/app/ui/App.tsx
@@ -4,6 +4,7 @@ import { Header, Layout } from "@/common/components"
 import { Routing } from "@/common/routing"
 import { GlobalLinearProgress } from "@/common/components/GlobalLinearProgress/GlobalLinearProgress"
 import { ErrorToaster } from "@/common/components/ErrorToaster/ErrorToaster"
+import { NavBar } from "@/common/components/NavBar/NavBar.tsx"
 
 export const App = () => {
   return (
@@ -12,7 +13,10 @@ export const App = () => {
       <Header />
       <GlobalLinearProgress />
       <Layout>
-        <Routing />
+        <NavBar />
+        <main>
+          <Routing />
+        </main>
       </Layout>
       <ToastContainer />
     </>
